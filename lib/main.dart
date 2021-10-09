73d798ec5adc17c65b05074e8498f1deb604cdbf
Satya Srivatsav Gadiyaram <sathya@Satyas-MacBook-Air.local>
2021-10-09 20:03:56 +0530

    ChooseScreenDone

--- lib/main.dart
+++ lib/main.dart
@@ -1,20 +1,48 @@
+import 'package:firebase_auth/firebase_auth.dart';
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'package:gamer_street/screens/auth_screen.dart';
+import 'package:gamer_street/screens/choose_screen.dart';
+import 'package:gamer_street/screens/games_screen.dart';
+import 'package:gamer_street/screens/splash_screen.dart';
 
 void main() {
   runApp(MyApp());
 }
 
 class MyApp extends StatelessWidget {
+  static const String appName = "GamerSteet";
   // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      title: 'Flutter Demo',
-      theme: ThemeData(
-        primarySwatch: Colors.blue,
-      ),
-      home: AuthScreen(),
+    return FutureBuilder(
+      builder: (ctx, snapShot) {
+        if (snapShot.connectionState == ConnectionState.done) {
+          return MaterialApp(
+            title: 'Flutter Demo',
+            theme: ThemeData(
+              primarySwatch: Colors.blue,
+            ),
+            home: StreamBuilder(
+              stream: FirebaseAuth.instance.authStateChanges(),
+              builder: (ctx, userSnapshot) {
+                if (userSnapshot.connectionState == ConnectionState.waiting) {
+                  return SplashScreen();
+                }
+                if (userSnapshot.hasData) {
+                  return GamesScreen();
+                }
+                return ChooseScreen();
+              },
+            ),
+            routes: {
+              AuthScreen.authScreenRoute: (ctx) => AuthScreen(),
+            },
+          );
+        }
+        return SplashScreen();
+      },
+      future: Firebase.initializeApp(),
     );
   }
 }
--- lib/screens/auth_screen.dart
+++ lib/screens/auth_screen.dart
@@ -1,6 +1,8 @@
+import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
 
 class AuthScreen extends StatefulWidget {
+  static const String authScreenRoute = "/auth-screen";
   const AuthScreen({Key? key}) : super(key: key);
 
   @override
@@ -8,14 +10,19 @@ class AuthScreen extends StatefulWidget {
 }
 
 class _AuthScreenState extends State<AuthScreen> {
+  final _auth = FirebaseAuth.instance;
+  var _isLoading = false;
   @override
   Widget build(BuildContext context) {
+    var loginCheck =
+        ModalRoute.of(context)!.settings.arguments as Map<String, bool>;
+    bool check = loginCheck['val'] as bool;
     return Scaffold(
       appBar: AppBar(
         title: Text("GamerStreet"),
       ),
       body: Center(
-        child: Text("This is Gamer Steet"),
+        child: check ? Text("Login") : Text("SignUp"),
       ),
     );
   }

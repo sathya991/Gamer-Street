ca9fd75ff69cc9c45dcd974e215272d62a1d18d4
Satya Srivatsav Gadiyaram <sathya@Satyas-MacBook-Air.local>
2021-10-09 18:00:49 +0530

    Firebase Added

---
+++ lib/screens/auth_screen.dart
@@ -0,0 +1,22 @@
+import 'package:flutter/material.dart';
+
+class AuthScreen extends StatefulWidget {
+  const AuthScreen({Key? key}) : super(key: key);
+
+  @override
+  _AuthScreenState createState() => _AuthScreenState();
+}
+
+class _AuthScreenState extends State<AuthScreen> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text("GamerStreet"),
+      ),
+      body: Center(
+        child: Text("This is Gamer Steet"),
+      ),
+    );
+  }
+}

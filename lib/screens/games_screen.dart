e8b69cd32697cf14b2d8982d4259bb24401270e7
SASHiBHUMARAJU <sasi.bhumaraju@gmail.com>
2021-10-09 21:34:28 +0530

    --Login-- widget is added to --lib/Widgets--

--- lib/screens/games_screen.dart
+++ lib/screens/games_screen.dart
@@ -5,6 +5,6 @@ class GamesScreen extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Container();
+    return Container(child: Text("this is login"));
   }
 }
